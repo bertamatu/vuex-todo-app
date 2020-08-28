57732d8be7cc433848f1ed8ba1b75f3d8cf004fa
dev <dev@devs-MacBook-Pro.local>
2020-08-28 10:55:59 +0300

    mark as complete works

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,10 +1,25 @@
 <template>
     <div>
         <h4>Todos</h4>
+        <div class="legend">
+            <span>Click to mark as complete</span>
+            <span>
+                <span class="incomplete-box"> = Incomplete</span>
+            </span>
+            <span>
+                <span class="complete-box"> = Complete</span>
+            </span>
+        </div>
+
         <div class="todos">
-            <div v-for="todo in allTodos" :key="todo.id" class="todo">
-                {{todo.title}}
-                <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
+            <div 
+            @click="onDblClick(todo)" 
+            v-for="todo in allTodos" 
+            :key="todo.id" 
+            class="todo" 
+            v-bind:class="{'is-complete':todo.completed}">
+            {{todo.title}}
+            <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
             </div>
         </div>
     </div>
@@ -16,7 +31,15 @@ import {mapGetters, mapActions} from 'vuex';
 export default {
     name: 'Todos',
     methods: {
-        ...mapActions(["fetchTodos", "deleteTodo"]),
+        ...mapActions(["fetchTodos", "deleteTodo", "updateTodo"]),
+        onDblClick(todo) {
+            const updTodo = {
+                id: todo.id,
+                title: todo.title,
+                completed: !todo.completed
+            }
+            this.updateTodo(updTodo);
+        }
     },
     // COMPUTED - to define which getters to use
     computed: mapGetters(['allTodos']),
@@ -41,6 +64,7 @@ export default {
     text-align: center;
     position: relative;
     cursor: pointer;
+    font-size: 0.6rem;
 }
 i {
     position: absolute;
@@ -48,5 +72,31 @@ i {
     right: 10px;
     color: rgb(255, 255, 255);
     cursor: pointer;
+}
+.legend {
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 1rem;
+}
+.complete-box {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background: rgb(148, 252, 200);
+}
+.incomplete-box {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background: rgb(255, 123, 119);
+}
+
+.is-complete {
+    background: rgb(144, 233, 144);
+}
+@media (max-width: 500px) {
+    .todos {
+        grid-template-columns: 1fr;
+    }
 }
 </style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -26,16 +26,28 @@ const actions = {
     },
     async filterTodos({commit}, e){
         //Get selected number
-        const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText);
+        // const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText);
+        const limit = e.target.value;
         const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`);
         commit('setTodos', response.data);
+    },
+    async updateTodo({commit}, updTodo){
+        const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${updTodo.id}`, updTodo);
+        console.log(response.data);
+        commit("updateTodo", response.data);
     }
 };
 
 const mutations = { 
-    setTodos: (state, todos)=> (state.todos = todos),
+    setTodos: (state, todos)=> state.todos = todos,
     newTodo: (state, todo) => state.todos.unshift(todo),
     removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id),
+    updateTodo: (state, updTodo) => {
+        const index = state.todos.findIndex(todo => todo.id === updTodo.id);
+        if(index !== -1) {
+            state.todos.splice(index, 1, updTodo)
+        }
+    }
 };
 
 export default {
@@ -44,6 +56,5 @@ export default {
     actions,
     mutations
 }
-
 
 //this is a boilerplate for vuex module
